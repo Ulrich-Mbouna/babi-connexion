281f83b67e14f7ca5b0e723739bb775a42dfd33c
Mbouna Ulrich <60409730+Ulrich15@users.noreply.github.com>
2024-06-01 00:57:06 +0100

    Ajout de la page d'erreur
    - Ajout d'une page d'erreur lorsqu'une ressource n'est pas trouvée dans l'application
    
    Signed-off-by: Mbouna Ulrich <60409730+Ulrich15@users.noreply.github.com>

---
+++ error.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type {NuxtError} from "#app";
+
+defineProps({
+  error: Object as () => NuxtError,
+})
+const handleError = () => clearError({redirect: '/'})
+</script>
+
+<template>
+<section class="uk-section uk-padding-remove-vertical uk-height-viewport uk-flex uk-flex-center uk-flex-middle">
+  <div class="uk-container uk-container-xsmall">
+    <div class="uk-width-large@s" uk-toggle="mode:media; media:@m; cls:uk-width-large@s uk-width-medium">
+      <img src="/images/error.png" alt="Error Logo" />
+    </div>
+    <div class="text-center uk-margin">
+      <h1>{{error.statusCode}}</h1>
+      <p>{{error.message}}</p>
+      <div class="" v-if="error.stack" v-html="error.stack"></div>
+    </div>
+    <div class="uk-margin text-center">
+      <button class="uk-button uk-button-primary uk-width-medium rounded" @click="handleError" >Retour à l'accueil</button>
+    </div>
+  </div>
+</section>
+</template>
+
+<style scoped>
+
+</style>
