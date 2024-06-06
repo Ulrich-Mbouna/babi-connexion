7a9d99592e81a5d02eea3a61b64ca43a9e5120cc
Mbouna Ulrich <60409730+Ulrich15@users.noreply.github.com>
2024-06-06 18:02:36 +0100

    Ajout de la vue de dashbaord client
    - Ajout de la vue de dashbaord client
    - Ajout d'un layout pour la gestion du dashboard
    - Extraction du menu du user connecté
    - Définition du script de UIkit uniquement pour le coté client
    - Réorganisation du dashboard
    - Ajout de quelques styles
    - Autres fix mineurs
    
    Signed-off-by: Mbouna Ulrich <60409730+Ulrich15@users.noreply.github.com>

---
+++ components/UserMenu.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="">
+    <div class="left-logo uk-flex uk-flex-middle uk-flex-center my-1">
+      <img class="custom-logo" src="/images/baby-connexion-logo.svg" alt="">
+    </div>
+    <div class="left-content-box">
+      <img src="/images/ulrich_avatar.png" alt="" class="uk-border-circle uk-width-small profile-img">
+      <h4 class="uk-text-center uk-margin-remove-vertical text-light font-medium">{{ user.firstName + ' ' + user.name }}</h4>
+
+      <div class="uk-position-relative uk-text-center uk-display-block uk-hidden">
+        <a href="#" class="uk-text-small uk-text-muted uk-display-block uk-text-center"
+           data-uk-icon="icon: triangle-down; ratio: 0.7">Admin</a>
+        <!-- user dropdown -->
+        <div class="uk-dropdown user-drop"
+             data-uk-dropdown="mode: click; pos: bottom-center; animation: uk-animation-slide-bottom-small; duration: 150">
+          <ul class="uk-nav uk-dropdown-nav uk-text-left">
+            <li><a href="#"><span data-uk-icon="icon: info"></span> Summary</a></li>
+            <li><a href="#"><span data-uk-icon="icon: refresh"></span> Edit</a></li>
+            <li><a href="#"><span data-uk-icon="icon: settings"></span> Configuration</a></li>
+            <li class="uk-nav-divider"></li>
+            <li><a href="#"><span data-uk-icon="icon: image"></span> Your Data</a></li>
+            <li class="uk-nav-divider"></li>
+            <li><a href="#"><span data-uk-icon="icon: sign-out"></span> Sign Out</a></li>
+          </ul>
+        </div>
+        <!-- /user dropdown -->
+      </div>
+    </div>
+    <div class="left-nav-wrap">
+      <ul class="uk-nav uk-nav-secondary uk-nav-parent-icon uk-margin-lef" data-uk-nav>
+        <li class="uk-nav-header font-bold">Mon Espace</li>
+        <li><a href="#"><span data-uk-icon="icon: comments" class="uk-margin-small-right"></span>Demande</a></li>
+        <li><a href="#"><span data-uk-icon="icon: users" class="uk-margin-small-right"></span>Visites</a></li>
+        <li class="uk-parent"><a href="#"><span data-uk-icon="icon: thumbnails" class="uk-margin-small-right"></span>Propositions</a>
+          <ul class="uk-nav-sub">
+            <li><a title="Article" href="https://zzseba78.github.io/Kick-Off/article.html">Article</a></li>
+            <li><a title="Album" href="https://zzseba78.github.io/Kick-Off/album.html">Album</a></li>
+            <li><a title="Cover" href="https://zzseba78.github.io/Kick-Off/cover.html">Cover</a></li>
+            <li><a title="Cards" href="https://zzseba78.github.io/Kick-Off/cards.html">Cards</a></li>
+            <li><a title="News Blog" href="https://zzseba78.github.io/Kick-Off/newsBlog.html">News Blog</a></li>
+            <li><a title="Price" href="https://zzseba78.github.io/Kick-Off/price.html">Price</a></li>
+            <li><a title="Login" href="https://zzseba78.github.io/Kick-Off/login.html">Login</a></li>
+            <li><a title="Login-Dark" href="https://zzseba78.github.io/Kick-Off/login-dark.html">Login - Dark</a></li>
+          </ul>
+        </li>
+        <li><a href="#"><span data-uk-icon="icon: album" class="uk-margin-small-right"></span>Paiements</a></li>
+        <li><a href="#"><span data-uk-icon="icon: thumbnails" class="uk-margin-small-right"></span>Rapport & Stats</a>
+        </li>
+      </ul>
+      <div class="left-content-box uk-margin-top hidden">
+        <h5>Daily Reports</h5>
+        <div>
+          <span class="uk-text-small">Traffic <small>(+50)</small></span>
+          <progress class="uk-progress" value="50" max="100"></progress>
+        </div>
+        <div>
+          <span class="uk-text-small">Income <small>(+78)</small></span>
+          <progress class="uk-progress success" value="78" max="100"></progress>
+        </div>
+        <div>
+          <span class="uk-text-small">Feedback <small>(-12)</small></span>
+          <progress class="uk-progress warning" value="12" max="100"></progress>
+        </div>
+
+      </div>
+
+    </div>
+    <div class="bar-bottom">
+      <ul class="uk-subnav uk-flex uk-flex-center uk-child-width-1-5" data-uk-grid>
+        <li>
+          <a href="#" class="uk-icon-link" data-uk-icon="icon: home" title="Home" data-uk-tooltip></a>
+        </li>
+        <li>
+          <a href="#" class="uk-icon-link" data-uk-icon="icon: settings" title="Settings" data-uk-tooltip></a>
+        </li>
+        <li>
+          <a href="#" class="uk-icon-link" data-uk-icon="icon: social" title="Social" data-uk-tooltip></a>
+        </li>
+
+        <li>
+          <a href="#" class="uk-icon-link" data-uk-tooltip="Sign out" data-uk-icon="icon: sign-out"></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script setup>
+defineOptions({
+  name: 'UserMenu'
+})
+
+const {data:user} = useAuth()
+</script>
---
+++ layouts/authenticated.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import "~/assets/js/card.js";
+import "~/assets/js/chartScript.js";
+
+const {data:user, status} = useAuth()
+
+const isLoggedIn = computed(() => status.value === 'authenticated')
+</script>
+
+<template>
+  <!--HEADER-->
+  <header id="top-head" class="uk-position-fixed">
+    <div class="uk-container uk-container-expand bg-primary/80">
+      <nav class="uk-navbar" data-uk-navbar="mode:click; duration: 250">
+        <div class="uk-navbar-left">
+          <div class="uk-navbar-item uk-hidden@m">
+            <a class="uk-logo" href="#">
+              <img class="custom-logo" src="/images/baby-connexion-logo.svg" alt=""></a>
+          </div>
+          <ul class="uk-navbar-nav uk-visible@m uk-hidden">
+            <li><a href="#">Accounts</a></li>
+            <li>
+              <a href="#">Settings <span data-uk-icon="icon: triangle-down"></span></a>
+              <div class="uk-navbar-dropdown">
+                <ul class="uk-nav uk-navbar-dropdown-nav">
+                  <li class="uk-nav-header">YOUR ACCOUNT</li>
+                  <li><a href="#"><span data-uk-icon="icon: info"></span> Summary</a></li>
+                  <li><a href="#"><span data-uk-icon="icon: refresh"></span> Edit</a></li>
+                  <li><a href="#"><span data-uk-icon="icon: settings"></span> Configuration</a></li>
+                  <li class="uk-nav-divider"></li>
+                  <li><a href="#"><span data-uk-icon="icon: image"></span> Your Data</a></li>
+                  <li class="uk-nav-divider"></li>
+                  <li><a href="#"><span data-uk-icon="icon: sign-out"></span> Logout</a></li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+          <div class="uk-navbar-item uk-visible@s uk-hidden">
+            <form action="" class="uk-search uk-search-default">
+              <span data-uk-search-icon></span>
+              <input class="uk-search-input search-field" type="search" placeholder="Search">
+            </form>
+          </div>
+        </div>
+        <div class="uk-navbar-right">
+          <ul class="uk-navbar-nav [&_li_*]:text-white">
+            <li><a href="#" data-uk-icon="icon:user" title="Your profile" data-uk-tooltip></a></li>
+            <li><a href="#" data-uk-icon="icon: settings" title="Settings" data-uk-tooltip></a></li>
+            <li><a href="#" data-uk-icon="icon:  sign-out" title="Sign Out" data-uk-tooltip></a></li>
+            <li class="uk-hidden@s"><a class="uk-navbar-toggle" data-uk-toggle data-uk-navbar-toggle-icon href="#offcanvas-nav" title="Offcanvas" data-uk-tooltip></a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <!--/HEADER-->
+  <!-- LEFT BAR -->
+  <aside id="left-col" class="uk-visible@m">
+    <UserMenu />
+  </aside>
+  <!-- /LEFT BAR -->
+  <!-- CONTENT -->
+  <div id="content" data-uk-height-viewport="expand: true">
+    <div class="uk-container uk-container-expand">
+      <div class="">
+        <slot />
+      </div>
+      <footer class="uk-section uk-section-small uk-text-center">
+        <hr>
+        <p class="uk-text-small uk-text-center">Copyright 2024 - Created by  <a class="text-primary" href="https://github.com/zzseba78/Kick-Off">Baby Connexion</a></p>
+      </footer>
+    </div>
+  </div>
+  <!-- /CONTENT -->
+  <!-- OFFCANVAS -->
+  <div id="offcanvas-nav" data-uk-offcanvas="flip: false; overlay: true">
+    <div class="uk-offcanvas-bar uk-offcanvas-bar-animation uk-offcanvas-slide">
+      <button class="uk-offcanvas-close uk-close uk-icon" type="button" data-uk-close></button>
+      <UserMenu/>
+    </div>
+  </div>
+  <!-- /OFFCANVAS -->
+</template>
+<style>
+@import "assets/css/dashboard.css";
+@import "assets/css/card.css";
+</style>
+
